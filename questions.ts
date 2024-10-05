bbb409cc288a2ed2ccf18be0b64da9500abde9bd
Nick Rafael Salas Diaz <nickrafaeldz@gmail.com>
2024-10-04 21:23:50 -0500

    Update questions.ts

--- questions.ts
+++ questions.ts
@@ -1,8 +1,5 @@
 export const quizData = [
     {
-        question: "What is the capital of Canada?",
-        options: ["Ottawa", "Toronto", "Calgary", "Edmonton"],
-        answer: "Ottawa"
         question: "¿Qué país ha ganado la Copa del Mundo más veces?",
         options: ["Brasil", "Alemania", "Italia", "Argentina"],
         answer: "Brasil"
@@ -16,65 +13,41 @@ export const quizData = [
         answer: "Lionel Messi"
     },
     {
-        question: "Who wrote 'To Kill a Mockingbird'?",
-        options: ["Harper Lee", "Mark Twain", "J.K. Rowling", "Stephen King"],
-        answer: "Harper Lee"
         question: "¿En qué continente se encuentra la sede de la FIFA?",
         options: ["Europa", "Asia", "África" , "América"],
         answer: "Europa"
     },
     {
-        question: "Which planet is known as the 'Red Planet'?",
-        options: ["Mars", "Venus", "Jupiter", "Mercury"],
-        answer: "Mars"
         question: "¿Quién ganó el Balón de Oro en 2018?",
         options: ["Luka Modrić", "Lionel Messi", "Robert Lewandowski", "Cristiano Ronaldo"],
         answer: "Luka Modrić"
     },
     {
-        question: "Who painted the Mona Lisa?",
-        options: ["Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "Claude Monet"],
-        answer: "Leonardo da Vinci"
         question: "¿En qué año ganó Perú su primera Copa América?",
         options: ["1939", "1986", "1998", "1978"],
         answer: "1939"
     },
     {
-        question: "What is the capital of Japan?",
-        options: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
-        answer: "Tokyo"
         question: "¿Qué equipo ganó la Champions League en 2020?",
         options: [" Real Madrid", "Manchester City", "Bayern Múnich", "Liverpool"],
         answer: "Bayer Munich"
     },
     {
-        question: "Who is the author of '1984'?",
-        options: ["George Orwell", "Aldous Huxley", "Ray Bradbury", "J.R.R. Tolkien"],
-        answer: "George Orwell"
         question: "¿Qué jugador ha ganado el mayor número de títulos de la UEFA Champions League?",
         options: ["Paco Gento", "Lionel Messi", "Cristiano Ronaldo", "Alfredo Di Stéfano"],
         answer: "Paco Gento"
     },
     {
-        question: "What is the largest organ in the human body?",
-        options: ["Liver", "Heart", "Skin", "Lung"],
-        answer: "Skin"
         question: "¿En qué país se jugó el segundo Mundial de fútbol?",
         options: [" Argentina", "Uruguay", "Italia", "Brasil"],
         answer: "Italia"
     },
     {
-        question: "Which continent is the largest by land area?",
-        options: ["Asia", "Africa", "North America", "Europe"],
-        answer: "Asia"
         question: "¿Cuántos equipos participan en la fase de grupos de la UEFA Champions League?",
         options: ["32", "24", "16", "48"],
         answer: "32"
     },
     {
-        question: "Who invented the telephone?",
-        options: ["Alexander Graham Bell", "Thomas Edison", "Nikola Tesla", "Albert Einstein"],
-        answer: "Alexander Graham Bell"
         question: "¿Quién fue el máximo goleador de la Copa del Mundo 2022?",
         options: ["Mbappe", "Lionel Messi", "Ronaldo",  "Thomas Müller"],
         answer: "Mbappe"
